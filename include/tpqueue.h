092874bb1afd73f08c4d170d89434a855c8b0940
z3m1ya <96194885+z3m1ya@users.noreply.github.com>
2022-04-29 22:35:03 +0300

    тупой анекдот в описании
    сидят в комнате двое, первый второму
    - зачем вам зонт?
     - а вдруг дождь
    - я впервые вижу человека который боится дождя в помещении
    - а я и не боюсь, у меня ведь есть зонт

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,9 +4,57 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+  struct ITEM {
+    T value;
+    ITEM* next, * prev;
+  };
+  ITEM* head, * tail;
+  TPQueue::ITEM* create(const T& value) {
+    ITEM* item = new ITEM;
+    item->value = value;
+    item->next = nullptr;
+    item->prev = nullptr;
+    return item;
+  }
+  
+  public:
+  TPQueue() : head(nullptr), tail(nullptr) {}
 
+  void push(const T& value) {
+        ITEM* t = head;
+        ITEM* item = create(value);
+        while (t && t->value.prior >= value.prior)
+            t = t->next;
+        if (!t && head) {
+            tail->next = item;
+            item->prev = tail;
+            tail = item;
+        } else if (!t && !head) {
+            head = tail = item;
+        } else if (!t->prev) {
+            head->prev = item;
+            item->next = head;
+            head = item;
+        } else {
+            t->prev->next = item;
+            item->prev = t->prev;
+            item->next = t;
+            t->prev = item;
+        }
+    }
+  T pop() {
+    if (!head || !tail)
+      throw std::string("error!");
+    ITEM* t = head->next;
+    if (t)
+      t->prev = nullptr;
+    T value = head->value;
+    head = t;
+    if (!head)
+      t = nullptr;
+    return value;
+  }
+};
 struct SYM {
   char ch;
   int prior;
