71acfb57a6e13601ec5ec6320d6cafd490676d9e
z3m1ya <96194885+z3m1ya@users.noreply.github.com>
2022-05-01 20:48:30 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,63 +1,58 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
-template<typename T>
+tlate<typename T>
 class TPQueue {
-  struct ITEM {
-    T value;
-    ITEM* next, * prev;
-  };
-  ITEM* head, * tail;
-  TPQueue::ITEM* create(const T& value) {
-    ITEM* item = new ITEM;
-    item->value = value;
-    item->next = nullptr;
-    item->prev = nullptr;
-    return item;
-  }
+private:
+    struct ITEM {
+        T value;
+        ITEM* next, * prev;
+    };
+    ITEM* head, * tail;
+    TPQueue::ITEM* create(const T& value) {
+        ITEM* item = new ITEM;
+        item->value = value;
+        item->next = item->prev = nullptr;
+        return item;
+    }
 
-  public: 
-  TPQueue() : head(nullptr), tail(nullptr) {}
-
-  void push(const T& value) {
+public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    void push(const T& value) {
         ITEM* t = head;
         ITEM* item = create(value);
-        while (t->value.prior >= value.prior && t)
+        while (t && t->value.prior >= value.prior)
             t = t->next;
-        if (!t && !head) {
-            head = tail = item;
-        } else if (!t && head) {
+        if (!t && head) {
             tail->next = item;
             item->prev = tail;
             tail = item;
-        } else if (!t->prev) {
+        }
+        else if (!t && !head) {
+            head = tail = item;
+        }
+        else if (!t->prev) {
             head->prev = item;
             item->next = head;
             head = item;
-        } else {
+        }
+        else {
             t->prev->next = item;
             item->prev = t->prev;
             item->next = t;
             t->prev = item;
         }
     }
-  T pop() {
-    if (!head || !tail)
-      throw int(1);
-    ITEM* t = head->next;
-    if (t)
-      t->prev = nullptr;
-    T value = head->value;
-    head = t;
-    if (!head)
-      t = nullptr;
-    return value;
-  }
+    T pop() {
+        ITEM* t = head->next;
+        if (t)
+            t->prev = nullptr;
+        T value = head->value;
+        delete head;
+        head = t;
+        if (!head)
+            tail = nullptr;
+        return value;
+    }
 };
-struct SYM {
-  char ch;
-  int prior;
-};
-
-#endif  // INCLUDE_TPQUEUE_H_
