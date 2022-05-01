1ceabfbaec9563b82cf46c137d5039773b5e88d9
z3m1ya <96194885+z3m1ya@users.noreply.github.com>
2022-05-01 22:36:47 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,3 +1,4 @@
+// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
@@ -18,33 +19,29 @@ private:
 
 public:
     TPQueue() : head(nullptr), tail(nullptr) {}
-    ~TPQueue() {
-        while (head)
-            pop();
-    }
     void push(const T& value) {
         ITEM* t = head;
         ITEM* item = create(value);
         while (t && t->value.prior >= value.prior)
             t = t->next;
-        if (!t && head) {
+        if (!t && !head) {
+            head = tail = item;
+        }
+        else if (!t && head) {
             tail->next = item;
             item->prev = tail;
             tail = item;
         }
-        else if (!t && !head) {
-            head = tail = item;
-        }
         else if (!t->prev) {
-            head->prev = item;
             item->next = head;
+            head->prev = item;
             head = item;
         }
         else {
             t->prev->next = item;
             item->prev = t->prev;
-            item->next = t;
             t->prev = item;
+            item->next = t;
         }
     }
     T pop() {
